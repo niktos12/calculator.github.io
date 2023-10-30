99033379e27258dbbfe71ada5a5b640a4188ccb9
niktos12 <nikitosss121207@gmail.com>
2023-10-30 17:35:38 +0400

    final

--- calculator/index.js
+++ calculator/index.js
@@ -6,28 +6,46 @@ const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
 const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
+const clear = document.getElementById('clear');
  
 add.addEventListener('click', () =>{
     result.value = Number(firstNumber.value) + Number(secondNumber.value);
     zeros();
+    redColorDelete()
 })
 subtract.addEventListener('click', ()=>{
     result.value = Number(firstNumber.value) - Number(secondNumber.value);
     zeros();
+    redColorDelete()
 })
 multiply.addEventListener('click', ()=>{
     result.value = Number(firstNumber.value) * Number(secondNumber.value);
     zeros();
+    redColorDelete()
 })
 divide.addEventListener('click', ()=>{
     result.value = Number(firstNumber.value) / Number(secondNumber.value);
     zeros();
+    redColorDelete()
 })
+clear.addEventListener('click', () =>{
+    result.value = "";
+    firstNumber.value = "";
+    secondNumber.value = "";
+    redColorDelete()
+});
 function zeros() {
     if (firstNumber.value == '' && secondNumber.value == ''){
     result.value = "Error";
     result.style.color = "red";
 }
+}
+function redColorDelete(){
+    if(result.value == "Error"){
+        result.style.color = "red";
+    }else{
+        result.style.color = "black";
+    }
 }
 
     
